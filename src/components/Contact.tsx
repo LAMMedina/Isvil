41a5e1fad8f7540e0c606c4b9d2c0eaaeaa305db
Luis Mujica <luismujicamedina@gmail.com>
2024-12-18 08:47:28 -0500

    add number phone

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -65,7 +65,7 @@ export default function Contacto() {
                 className="w-5 h-5 mr-2 sm:mr-6">
                 <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"></path>
               </svg>
-              <span>+51 972 708 799</span>
+              <span className="mr-4">+51 972 708 799</span>  <span> +51 946 846 060 </span>
             </p>
             <p className="flex items-center">
               <svg
